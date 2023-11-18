c6a401d13a5b773eda57063f76f8ecc281a49442
Sebastian-Paucar <jspaucar1@espe.edu.ec>
2023-11-18 09:41:04 -0500

    Pruebas

--- js/header.js
+++ js/header.js
@@ -1,41 +1,38 @@
-
-  // Función para manejar el clic en el botón 1
-  document.getElementById('boton1').addEventListener('click', function() {
-    window.location.href = 'inicio';
-  });
-  // Función para manejar el clic en el botón 2
-  document.getElementById('boton2').addEventListener('click', function() {
-    window.location.href = 'body';
-  });
-  // Función para manejar el clic en el botón 2
-  document.getElementById('boton3').addEventListener('click', function() {
-    window.location.href = 'habilidades';
-  });
-  document.getElementById('boton4').addEventListener('click', function() {
-    window.location.href = 'proyectos';
-  });
-  // Función para manejar el clic en el botón 2
-  document.getElementById('boton5').addEventListener('click', function() {
-    window.location.href = 'contactos';
-  });
-
+// Función para manejar el clic en el botón 1
+document.getElementById('boton1').addEventListener('click', function () {
+      window.location.href = '..';
+});
+// Función para manejar el clic en el botón 2
+document.getElementById('boton2').addEventListener('click', function () {
+      window.location.href = 'body';
+});
+// Función para manejar el clic en el botón 2
+document.getElementById('boton3').addEventListener('click', function () {
+      window.location.href = 'habilidades';
+});
+document.getElementById('boton4').addEventListener('click', function () {
+      window.location.href = 'proyectos';
+});
+// Función para manejar el clic en el botón 2
+document.getElementById('boton5').addEventListener('click', function () {
+      window.location.href = 'contactos';
+});
 
 
+function cambiarColorAlHacerScroll() {
+      var scrollPos = window.scrollY;
+      var nuevaColor;
 
-  function cambiarColorAlHacerScroll() {
-    var scrollPos = window.scrollY;
-    var nuevaColor;
+      // Puedes ajustar los valores según tus preferencias
+      if (scrollPos > 30) {
+            nuevaColor = '#E0E0E0'; // Rojo
+      } else {
+            nuevaColor = '#FFFFFF'; // Blanco
+      }
 
-    // Puedes ajustar los valores según tus preferencias
-    if (scrollPos > 30) {
-      nuevaColor = '#E0E0E0'; // Rojo
-    } else {
-      nuevaColor = '#FFFFFF'; // Blanco
-    }
+      // Aplicar el nuevo color de fondo
+      document.body.style.backgroundColor = nuevaColor;
+}
 
-    // Aplicar el nuevo color de fondo
-    document.body.style.backgroundColor = nuevaColor;
-  }
-
-  // Agregar un listener para el evento de scroll
-  window.addEventListener('scroll', cambiarColorAlHacerScroll);
+// Agregar un listener para el evento de scroll
+window.addEventListener('scroll', cambiarColorAlHacerScroll);
